ba4bfb61b20f4841c90633cbc62d65144ffcd673
chenrentong <846722718@qq.com>
2021-12-15 22:31:52 +0800

    config

---
+++ Mapper/src/main/java/com/atguigu/mybatis/bean/Employee.java
@@ -0,0 +1,23 @@
+package com.atguigu.mybatis.bean;
+
+
+import lombok.*;
+
+/**
+ * @author chenrentong
+ * @create 2021-12-13 18:04
+ */
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+//@Alias("emp")
+public class Employee {
+
+    private Integer id;
+    private String lastName;
+    private String email;
+    private String gender;
+
+}
---
+++ Mapper/src/main/java/com/atguigu/mybatis/dao/EmployeeMapper.java
@@ -0,0 +1,22 @@
+package com.atguigu.mybatis.dao;
+
+import com.atguigu.mybatis.bean.Employee;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
+
+public interface EmployeeMapper {
+
+	public Employee getEmpByMap(Map<String, Object> map);
+
+	public Employee getEmpByIdAndLastName(@Param("id")Integer id, @Param("lastName")String lastName);
+	
+	public Employee getEmpById(Integer id);
+
+	public long addEmp(Employee employee);
+
+	public boolean updateEmp(Employee employee);
+
+	public boolean deleteEmpById(Integer id);
+
+}
